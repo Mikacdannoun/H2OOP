3609e705cc3127e1311249fd4462990b4fd4cc6a
MikaDannoun <mikacdannoun@gmail.com>
2024-04-15 09:20:55 +0200

    4.1 opg finished

---
+++ 4.1 opg/Reciter.cs
@@ -0,0 +1,40 @@
+﻿
+public class Reciter
+{
+    private static Random _rng = new Random();
+
+    /// <summary>
+    /// Recites a set of lists of words.
+    /// </summary>
+    public static void ReciteAllTheWords()
+    {
+        List<string> danish = new List<string> { "En", "To", "Tre", "Fire", "Fem", "Seks", "Syv", "Otte" };
+        List<string> english = new List<string> { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight" };
+        List<string> german = new List<string> { "Eins", "Zwei", "Drei", "Vier", "Funf", "Sechs", "Sieben", "Acht" };
+
+        //Recite(danish);
+        //Recite(english);
+        //Recite(german);
+
+        Task taskDanish = Task.Run(() => Recite(danish)) ;
+        Task taskEnglish = Task.Run(() => Recite(english));
+        Task taskGerman = Task.Run(() => Recite(german));
+
+        //The WriteLine in program.cs was written before any recited numbers, so i use await here in ReciteAllTheWords
+        //In order to make program.cs wait until all numbers have been recited to write in the console
+        Task.WaitAll(taskDanish, taskEnglish, taskGerman);
+    }
+
+    /// <summary>
+    /// Recites (i.e. prints on screen with a bit of delay
+    /// between each line) the provided list of strings.
+    /// </summary>
+    public static void Recite(List<string> words)
+    {
+        foreach (string s in words)
+        {
+            Console.WriteLine(s);
+            Thread.Sleep(_rng.Next(1000) + 50);
+        }
+    }
+}
